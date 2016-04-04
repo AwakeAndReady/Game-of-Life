706d57d86124aa5b1bdfd026562ba0ffa2fbcfb7
AwakeAndReady <rene.sesgoer@gmail.com>
2016-04-04 13:58:18 +0200

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -1,8 +1,14 @@
 package gameOfLife4;
 
+import java.awt.Point;
 import java.util.Scanner;
 
 public class GameController {
+  
+  /*
+   * Bleibt möglichst dumm, kümmert sich nicht um den Spielablauf.
+   * Steuert die Ein und Ausgabe an sich.
+   */
   
   private Scanner inputScanner = new Scanner( System.in );
   
@@ -19,6 +25,36 @@ public class GameController {
       default:
         return null;
     }
+  }
+  
+  public void draw(Playground pg) {
+    // Raster mit Figuren zeichnen
+    for ( int y = 0; y < pg.getRows(); y++ ) {
+      for ( int x = 0; x < pg.getCollums(); x++ ) {
+        Point p = pg.getPoint(x, y);
+        if ( pg.getPlayer().isAtPosition( p ) )
+          System.out.print( pg.getPlayer().getIcon() );
+        else if ( pg.getSnake().isAtPosition( p ) )
+          System.out.print( pg.getSnake().getIcon() );
+        else if ( pg.getGold().isAtPosition( p ) )
+          System.out.print( pg.getGold().getIcon() );
+        else if ( pg.getDoor().isAtPosition( p ) )
+          System.out.print( pg.getDoor().getIcon() );
+        else
+          System.out.print( '.' );
+      }
+      System.out.println();
+    }
+    
+    if ( pg.hasWon() ) {
+      System.out.println( "Gewonnen!" );
+      return;
+    }
+    
+    if ( pg.hasLost() ) {
+      System.out.println( "ZZZZZZZ. Die Schlange hat dich!" );
+    }
+    
   }
   
 }
